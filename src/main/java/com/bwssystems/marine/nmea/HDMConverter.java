6ec91b18d1d6cefb862c13c7133a73a2909877ac
BWS Systems <info.bwssystems@gmail.com>
2019-05-31 09:59:17 -0500

    Updated connection handling

--- src/main/java/com/bwssystems/marine/nmea/HDMConverter.java
+++ src/main/java/com/bwssystems/marine/nmea/HDMConverter.java
@@ -7,46 +7,56 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HDMConverter {
- 
+
     public static void main(String argv[]) throws Exception {
         String clientSentence;
         String targetSentence;
         ServerSocket welcomeSocket = new ServerSocket(6789);
         Logger log = LoggerFactory.getLogger(HDMConverter.class);
-        
+
         log.info("Starting HDMConverter....");
 
-        log.info("Waiting for connection...");
+        log.info("Waiting for connection....");
         Socket connectionSocket = welcomeSocket.accept();
-        log.info("Socket connected...");
+        log.info("Socket connected....");
         BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
         DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
         log.info("Start read loop....");
-        while (true) {
-            clientSentence = inFromClient.readLine();
-            targetSentence = SentenceConvert.doReplace(clientSentence, "$AP", "$HC");
-//            log.info("Debug client sentence: " + clientSentence);
-//            log.info("Debug target sentence: " + targetSentence);
-            if(!targetSentence.equals(clientSentence)) {
-                targetSentence = targetSentence + "\r\n";
-//                log.info("Debug output for converted sentence: " + targetSentence);
-                try {
-                    outToClient.writeBytes(targetSentence);
-                }
-                catch(Exception e) {
-                    log.error("Write exception occurred, shutting down.");
-                    break;
-                }
+        boolean running = true;
+        while (running) {
+            try {
+                clientSentence = inFromClient.readLine();
+                if (clientSentence != null && clientSentence.length() > 0) {
+                    targetSentence = SentenceConvert.doReplace(clientSentence, "$AP", "$HC");
+                    // log.info("Debug client sentence: " + clientSentence);
+                    // log.info("Debug target sentence: " + targetSentence);
+                    if (!targetSentence.equals(clientSentence)) {
+                        targetSentence = targetSentence + "\r\n";
+                        // log.info("Debug output for converted sentence: " + targetSentence);
+                        outToClient.writeBytes(targetSentence);
 
+                    } else {
+                        // log.info("Debug Not sending converted message for: " + clientSentence);
+                        // log.info(" with sentence: " + targetSentence);
+                    }
+                } else if(clientSentence == null) {
+                    log.warn("client sentence is null, resetting connection");
+                    inFromClient.close();
+                    outToClient.close();
+                    connectionSocket.close();
+                    throw(new IOException("We must have lost connection..."));
+                }
+            } catch (IOException e) {
+                log.error("Lost TCP connection, {}, resetting....", e.getMessage());
+                log.info("Waiting for TCP connection....");
+                connectionSocket = welcomeSocket.accept();
+                log.info("Socket connected....");
+                inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+                outToClient = new DataOutputStream(connectionSocket.getOutputStream());
             }
-            else {
-//                log.info("Debug Not sending converted message for: " + clientSentence);
-//                log.info("    with sentence: " + targetSentence);
-            }
-
         }
-        
-        log.info("HDMConverter shutting down...");
+
+        log.info("HDMConverter shutting down....");
         welcomeSocket.close();
         System.exit(0);
     }
